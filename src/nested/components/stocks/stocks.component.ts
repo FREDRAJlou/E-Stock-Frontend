e094033582dfcbc8d0128b676ea4d8133737a721
Fredrick <freddricklourduraj@gmail.com>
2022-01-05 12:01:07 +0530

    Build path changes

--- src/nested/components/stocks/stocks.component.ts
+++ src/nested/components/stocks/stocks.component.ts
@@ -37,9 +37,7 @@ export class StocksComponent implements OnInit {
     this.service.getAllCompanies("info/"+this.search.companyCode).subscribe(data=>{
       console.log(data);
       this.companies = data["companyList"];
-      this.company = this.companies[0];
-      this.stocks=this.company.stocks;
-  
+      this.company = this.companies[0]; 
     if(this.search.startDate && this.search.endDate){
     this.service.getAllStocks('get/'+this.search.companyCode+"/"+formatDate(this.search.startDate, 'dd-MM-yyyy' ,this.locale)+"/"+ formatDate(this.search.endDate, 'dd-MM-yyyy' ,this.locale)).subscribe(data=>{
       this.stocks=data["stockList"];
@@ -48,7 +46,9 @@ export class StocksComponent implements OnInit {
       this.maxPrice=data["maxPrice"];
       console.log(this.stocks);
     });
-  }
+}else{
+  this.stocks=this.company.stocks;
+}
 });
   }
 }
