c6433c83ff3332d45211fdd97cfaf772344825d6
Fredrick <freddricklourduraj@gmail.com>
2022-01-05 12:33:49 +0530

    Added toater message

--- src/nested/components/stocks/stocks.component.ts
+++ src/nested/components/stocks/stocks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Search } from 'src/nested/models/search.model';
 import { StockService } from 'src/nested/services/stock.service';
 import {  formatDate } from '@angular/common';
+import { MessageService } from 'primeng/api';
   
 import { Inject,  LOCALE_ID }  from '@angular/core';
 
@@ -11,33 +12,36 @@ import { Inject,  LOCALE_ID }  from '@angular/core';
   styleUrls: ['./stocks.component.scss']
 })
 export class StocksComponent implements OnInit {
-  search: Search={companyCode:"",startDate:new Date(),endDate:new Date};
+  search: any={companyCode:"",startDate:'',endDate:''};
   companies: any[]=[];
   company:any;
   stocks: any;
   minPrice:any;
   maxPrice:any;
   avgPrice:any;
-  constructor(public service : StockService,@Inject(LOCALE_ID) public locale: string) { }
+  constructor(public msgService : MessageService, public service : StockService,@Inject(LOCALE_ID) public locale: string) { }
 
   ngOnInit(): void {
 
   }
 
-  findCompany(){
+  findStocks(){
+    if(!this.search.companyCode||this.search.companyCode==''||this.search.companyCode==null){
+      this.msgService.add({severity:'warning', summary:'CompanyCode is required'+this.search.companyCode, detail:""});
+    }
     this.service.getAllCompanies("info/"+this.search.companyCode).subscribe(data=>{
       console.log(data);
       this.companies = data["companyList"];
+      if(this.companies.length==0){
+        this.msgService.add({severity:'warning', summary:'No Company Available for companyCode '+this.search.companyCode, detail:""});
+    
+      }else{
       this.company = this.companies[0];
       this.stocks=this.company.stocks;
-    });
-  }
-
-  findStocks(){
-    this.service.getAllCompanies("info/"+this.search.companyCode).subscribe(data=>{
-      console.log(data);
-      this.companies = data["companyList"];
-      this.company = this.companies[0]; 
+      if(this.companies.length!=0&&this.stocks?.length==0){
+        this.msgService.add({severity:'warning', summary:'No Stocks Available for '+this.search.companyCode, detail:""});
+      }
+    }
     if(this.search.startDate && this.search.endDate){
     this.service.getAllStocks('get/'+this.search.companyCode+"/"+formatDate(this.search.startDate, 'dd-MM-yyyy' ,this.locale)+"/"+ formatDate(this.search.endDate, 'dd-MM-yyyy' ,this.locale)).subscribe(data=>{
       this.stocks=data["stockList"];
